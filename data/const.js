bd54512d48610a450f95d9c29e501ed845c1368a
doraemon1013 <dorachann1013@gmail.com>
2025-07-10 12:50:47 +0900

    a

--- data/const.js
+++ data/const.js
@@ -5,8 +5,8 @@ const MAP_WIDTH = 70; //マップの横幅
 const MAP_HEIGHT = 40; //マップの縦幅
 const TILE_SIZE = 66; //タイルのサイズ
 const MAX_SPEED = 8; //最大移動速度
-const HERO_W = 32; //ヒーローの横幅
-const HERO_H = 32; //ヒーローの縦幅
+const HERO_W = 96; //ヒーローの横幅
+const HERO_H = 96; //ヒーローの縦幅
 const SQRT2 = Math.sqrt(2);
 const offset = {
         x:-521,
--- data/function.js
+++ data/function.js
@@ -2,8 +2,8 @@
 //便利な関数保存用
 //
 function iscollide(boundary){
-        return (boundary.loc.x < ((canvas.width>>1) + (HERO_W+2))&&
+        return (boundary.loc.x < ((canvas.width>>1) + (HERO_W))&&
                 boundary.loc.x + TILE_SIZE> ((canvas.width>>1))&&
-                boundary.loc.y < ((canvas.height>>1) + (HERO_H+2))&&
+                boundary.loc.y < ((canvas.height>>1) + (HERO_H))&&
                 boundary.loc.y + (TILE_SIZE>>1) > ((canvas.height>>1)))
     }//衝突しているかどうか
