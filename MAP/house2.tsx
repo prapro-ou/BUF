0f8fe69df12097e2c1236032ff12f84d4fabf5c5
oneplus-1 <lijiayi8521@gmail.com>
2025-07-19 18:23:44 +0900

    new file:   MAP/House (2) (1) (1).png 	new file:   MAP/House (2) (1) (1).png:Zone.Identifier 	new file:   MAP/House (2) (1).png 	new file:   MAP/House (2) (1).png:Zone.Identifier 	new file:   MAP/New Piskel (3).png 	new file:   MAP/New Piskel (3).png:Zone.Identifier 	new file:   MAP/Untitled 07-10-2025 09-00-54 (3) (3).png 	new file:   MAP/Untitled 07-10-2025 09-00-54 (3) (3).png:Zone.Identifier 	new file:   MAP/Untitled 07-10-2025 09-00-54 (4).png 	new file:   MAP/Untitled 07-10-2025 09-00-54 (4).png:Zone.Identifier 	new file:   MAP/house1.tsx 	new file:   MAP/house2.tsx 	new file:   MAP/house3.tsx 	new file:   MAP/house4.tsx 	modified:   MAP/main_map.tmx 	new file:   MAP/main_mate.tsx

---
+++ MAP/house2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="house2" tilewidth="32" tileheight="32" tilecount="36" columns="6">
+ <image source="Untitled 07-10-2025 09-00-54 (3) (3).png" width="192" height="192"/>
+</tileset>
---
+++ MAP/main_mate.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="main_mate" tilewidth="32" tileheight="32" tilecount="399" columns="21">
+ <image source="main_mate.png" width="672" height="608"/>
+</tileset>
