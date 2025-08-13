1b5045e1ac148a8d2958466b47fc3fac7f842037
doraemon1013 <dorachann1013@gail.com>
2025-08-13 17:37:30 +0900

    a

---
+++ MapsForYuya/bridge_damage.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="bridge_damage (2)" tilewidth="32" tileheight="32" tilecount="8" columns="2">
+ <image source="../MAP素材置き場/bridge_damage (2).png" width="64" height="128"/>
+</tileset>
