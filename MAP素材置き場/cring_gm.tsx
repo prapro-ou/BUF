6edb959ec401430f00eea93bf6b23d7ffc86bab8
doraemon1013 <dorachann1013@gail.com>
2025-08-18 12:44:49 +0900

    a

--- MAP素材置き場/cring_gm.tsx
+++ MAP素材置き場/cring_gm.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="cring_gm" tilewidth="32" tileheight="32" tilecount="12" columns="12">
+<tileset version="1.10" tiledversion="1.11.2" name="cring_gm" tilewidth="32" tileheight="32" tilecount="3" columns="3">
  <grid orientation="orthogonal" width="32" height="64"/>
- <image source="map (1).png" width="384" height="32"/>
+ <image source="../img/effect/nobirukusa.png" width="96" height="32"/>
 </tileset>
