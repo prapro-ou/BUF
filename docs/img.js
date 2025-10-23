181d82c3ab8ef18430d5eae387bb97fba525114b
doraemon1013 <dorachann1013@gail.com>
2025-10-23 23:30:52 +0900

    imgをdocに移動

--- docs/img.js
+++ docs/img.js
@@ -2,27 +2,27 @@
 //画像読み込み用
 //
 const bgImage2 = new Image()
-bgImage2.src = '../img/Maps/bg_default2.png'
+bgImage2.src = 'img/Maps/bg_default2.png'
 const bgImage = new Image()
-bgImage.src = '../img/Maps/bg_default.png'
+bgImage.src = 'img/Maps/bg_default.png'
 const fgImage = new Image()
-fgImage.src = '../img/Maps/fg_default.png'
+fgImage.src = 'img/Maps/fg_default.png'
 const shopImage = new Image()
-shopImage.src = '../img/Maps/douguya.png'
+shopImage.src = 'img/Maps/douguya.png'
 const wait_icon = new Image()
-wait_icon.src = '../img/effect/!.png'
+wait_icon.src = 'img/effect/!.png'
 
 const playerImg_down = new Image()
-      playerImg_down.src = '../img/Character/heroDown.png'
+      playerImg_down.src = 'img/Character/heroDown.png'
 const playerImg_up = new Image()
-      playerImg_up.src = '../img/Character/heroUp.png'
+      playerImg_up.src = 'img/Character/heroUp.png'
 const playerImg_right = new Image()
-      playerImg_right.src = '../img/Character/heroRight.png'
+      playerImg_right.src = 'img/Character/heroRight.png'
 const playerImg_left = new Image()
-      playerImg_left.src = '../img/Character/heroLeft.png'
+      playerImg_left.src = 'img/Character/heroLeft.png'
 
 const heroImage = new Image()
-      heroImage.src = '../img/Character/hero.png'
+      heroImage.src = 'img/Character/hero.png'
 const playerImages = {
   down: new Image(),
   up: new Image(),
@@ -36,22 +36,22 @@ playerImages.left.src = playerImg_left.src;
 playerImages.right.src = playerImg_right.src;
 
 const kanbanNpcImage = new Image()
-      kanbanNpcImage.src = '../img/Character/kanbanNpc.png'
+      kanbanNpcImage.src = 'img/Character/kanbanNpc.png'
 const kusaNpcImage = new Image()
-      kusaNpcImage.src = '../img/Character/kusaNpc.png'
+      kusaNpcImage.src = 'img/Character/kusaNpc.png'
 const nobirukusa = new Image()
-      nobirukusa.src = '../img/effect/nobirukusa.png'
+      nobirukusa.src = 'img/effect/nobirukusa.png'
 const kusaBaba_cry = new Image()
-      kusaBaba_cry.src = '../img/Character/GranMa_cry.png'
+      kusaBaba_cry.src = 'img/Character/GranMa_cry.png'
 const kusaBaba_hpy = new Image()
-      kusaBaba_hpy.src = '../img/Character/GranMa_Happy.png'
+      kusaBaba_hpy.src = 'img/Character/GranMa_Happy.png'
 const hasiNpcImage = new Image()
-      hasiNpcImage.src = '../img/Character/hasiNpc.png'
+      hasiNpcImage.src = 'img/Character/hasiNpc.png'
 const BOSSImage = new Image()
-      BOSSImage.src = '../img/Character/npc_kuromaku.png'
+      BOSSImage.src = 'img/Character/npc_kuromaku.png'
 const treasureBoxImage = new Image()
-      treasureBoxImage.src = '../img/effect/BOX_close.png'
+      treasureBoxImage.src = 'img/effect/BOX_close.png'
 const treasureBoxImage2 = new Image()
-      treasureBoxImage2.src = '../img/effect/BOX_open.png'
+      treasureBoxImage2.src = 'img/effect/BOX_open.png'
 const kaziya = new Image();
-kaziya.src = '../img/Character/blacksmith.png'
+kaziya.src = 'img/Character/blacksmith.png'
